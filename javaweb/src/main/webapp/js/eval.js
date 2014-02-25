e6103202056a7c188d5ec77911019dcad408b586
cherishqq <haijinme@qq.com>
2014-02-25 14:51:24 +0800

    乱码

--- javaweb/src/main/webapp/js/eval.js
+++ javaweb/src/main/webapp/js/eval.js
@@ -9,3 +9,21 @@ function test2(){
 	eval("var i=3");
 	alert(i);
 }
+
+/**
+ 测试作用域。。
+ 可以访问外部变量
+**/
+
+var oo = {};
+var cc = "4";
+oo.some = function(){
+	alert(cc);
+}
+
+
+
+function test3(){
+	
+	eval("oo.some()");
+}
